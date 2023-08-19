224e1d64af00e2b5f71297f1fad46a7530677369
KostiantynPolishko1 <kostantinpolishko@gmail.com>
2023-08-19 09:39:23 +0300

    6. Has been realized function 'delete_data' inside the file json.

--- cosmo.py
+++ cosmo.py
@@ -24,17 +24,6 @@ def read_data(f_name: str, mode: str) -> list:
         return arr_data
 
 
-def save_data(f_name: str, arr_data: list, mode='w') -> None:
-    if exception.check_data(f_name):
-        with open(f_name, mode) as file:
-            json.dump(arr_data, file, indent=4)
-    else:
-        f_name = file_rename(f_name)
-        with open(f_name, mode) as file:
-            print('see new file \'{}\''.format(f_name))
-            json.dump(arr_data, file, indent=4)
-
-
 def list_id(ind: int, arr_data: list, name_fun: str) -> None:
     arr_key = list(dict.keys(arr_data[0]))
     print('\n', name_fun)
@@ -67,7 +56,7 @@ def add_data(arr_data: list, name_fun: str, f_name='') -> bool:
             continue
 
 
-def find_data(arr_data: list, name_fun: str, f_name='') -> bool:
+def find_data(arr_data: list, name_fun: str, function='') -> int:
 
     ind = 0
     while True:
@@ -75,18 +64,19 @@ def find_data(arr_data: list, name_fun: str, f_name='') -> bool:
         ind, operation = menu.receive_pos(len(arr_data) - 1, ind)
         menu.clean_s()
 
-        arr_key = list(dict.keys(arr_data[ind]))
         if not operation:
             print('data of object:')
             for key in arr_data[ind]:
                 print('\t{} -> {}'.format(key, arr_data[ind][key]))
 
-            if menu.exit_menu():
-                return False
-                # arr_data[ind][arr_key[0]]
+            if function == 'delete_data':
+                return ind
             else:
-                menu.clean_s()
-                continue
+                if menu.exit_menu():
+                    return 0
+                else:
+                    menu.clean_s()
+                    continue
 
 
 def modify_data(arr_data: list, name_fun: str, f_name='') -> bool:
@@ -95,8 +85,15 @@ def modify_data(arr_data: list, name_fun: str, f_name='') -> bool:
 
 
 def delete_data(arr_data: list, name_fun: str, f_name='') -> bool:
-    print(name_fun)
-    return False
+
+    while True:
+        ind = find_data(arr_data, name_fun, 'delete_data')
+        del_element = input('\n\tdel object -> ')
+        if not del_element:
+            del arr_data[ind]
+            menu.clean_s()
+            return False
+        menu.clean_s()
 
 
 def get_all_data(arr_data: list, name_fun: str, f_name='') -> bool:
@@ -106,5 +103,16 @@ def get_all_data(arr_data: list, name_fun: str, f_name='') -> bool:
 
 def exit_save(arr_data: list, name_fun: str, f_name) -> bool:
     print('\n\t', name_fun)
+
+    def save_data(_f_name: str, _arr_data: list, mode='w') -> None:
+        if exception.check_data(_f_name):
+            with open(_f_name, mode) as file:
+                json.dump(_arr_data, file, indent=4)
+        else:
+            _f_name = file_rename(_f_name)
+            with open(_f_name, mode) as file:
+                print('see new file \'{}\''.format(_f_name))
+                json.dump(_arr_data, file, indent=4)
+
     save_data(f_name, arr_data, 'w')
     return True
